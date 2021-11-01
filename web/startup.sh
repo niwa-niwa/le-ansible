93758db37f04dcc062d1600a89df849579f8c5e4
niwa-niwa <urban.hang274@gmail.com>
2021-11-02 00:57:41 +0900

    fixed relation of nginx & php-fpm

--- web/startup.sh
+++ web/startup.sh
@@ -5,3 +5,6 @@
 
 # Nginx起動
 /usr/sbin/nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
+# nginxリロード /usr/sbin/nginx -s reload
+# nginx停止 /usr/sbin/nginx -s stop
+# https://www.nginx.com/resources/wiki/start/topics/tutorials/commandline/
